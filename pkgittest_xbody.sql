f19721dd02dd267f29894a25a2af11f3b7427345
AkuomaH <63924660+AkuomaH@users.noreply.github.com>
2026-07-09 12:25:14 +0100

    First Commit (#7)
    * First Commit
    
    * Create Package body 2

---
+++ pkgittest_xbody.sql
@@ -0,0 +1,36 @@
+CREATE OR REPLACE PACKAGE BODY pkgittest AS
+--
+  /* Created By       : Akuoma Amaefule
+  ** Date Created     : 07-JULY-2026
+  ** Release Version  : 2.0
+  **
+  ** PURPOSE: GIT TEST
+  **
+  ** MODIFICATION HISTORY:
+  **
+  **    Modified By   :
+  **    Date          :
+  **    Version       :
+  **    Remarks       :
+  **
+  **    Modified By   :
+  **    Date          :
+  **    Version       :
+  **    Remarks       :
+  **  
+  **
+  */
+  --
+  PROCEDURE pr_logg (pi_txt VARCHAR2) IS
+  BEGIN
+    LOGGER.LOG(p_text => '[GITTEST]['||TO_CHAR(SYSDATE,'YYYY/MM/DD HH24:MI:SS')||']: '||pi_txt, 
+               p_scope => 'GIT'); 
+  --
+  -- 
+  FUNCTION fn_get_curr_dtt RETURN VARCHAR2 IS
+     BEGIN
+       RETURN TO_CHAR(SYSDATE, 'YYYY-MM-DD HH24:MI:SS');
+	END;
+
+END pkgittest;
+/
